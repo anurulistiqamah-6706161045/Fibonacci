5319dac45606c8d50824119b2cc1f69ca6762196
anurulistiqamah-6706161045 <37771613+anurulistiqamah-6706161045@users.noreply.github.com>
2018-03-26 09:10:54 +0700

    Fibonacci

---
+++ untitled/src/Fibonacci.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int tot, fibo = 1, awalan = 0;
+
+        System.out.print("How much ur input: ");
+        tot = in.nextInt();
+
+        if (tot > 0){
+            for (int i = 0; i < tot; i++) {
+                System.out.print(fibo + ", ");
+                int temp = fibo;
+                fibo += awalan; //fibo + 0
+                awalan = temp; // 0 = temp
+            }
+        }
+    }
+}
